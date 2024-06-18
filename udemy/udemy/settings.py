c69fba63d45715093ddfb66244b54695a053e21a
Jayesh Shinde <48760468+jayesh125@users.noreply.github.com>
2024-06-18 09:12:41 +0530

    Update settings.py

--- udemy/udemy/settings.py
+++ udemy/udemy/settings.py
@@ -129,6 +129,6 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jayesh.v.shinde1251@gmail.com'
-EMAIL_HOST_PASSWORD = 'bpzy kntx eocb ztbi'
+EMAIL_HOST_PASSWORD = '**** **** **** ****'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
